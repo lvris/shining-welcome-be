a0a3fb7ccc9bfe3a8ac485a90e8901877ddddcaf
lvris <lvris@qq.com>
2023-04-21 23:03:12 +0800

    feat: CURD guest with TS

--- src/interfaces/guest.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface Guest {
-  contactType: string,
-  contact: string,
-  name: string,
-  cname?: string,
-  school: string,
-  cos?: string,
-}
-
--- src/routers/guest.ts
+++ src/routers/guest.ts
@@ -1,11 +1,70 @@
 import express from 'express';
-import { guest } from '../../prisma';
+import prisma from '../../prisma';
+import { IPage } from '../common/interfaces/common.interface';
+import { IGuest, IGuestUpdate } from '../common/interfaces/guest.interface';
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  const all = await guest.findMany();
+/**
+ * 添加游客
+ */
+router.post<{},{},IGuest>('/', async (req, res) => {
+  const info = req.body;
+  const guest = await prisma.guest.create({
+    data: info
+  });
+  res.json(guest);
+})
+
+/**
+ * 获取游客列表
+ */
+router.get<IPage>('/', async (req, res) => {
+  const page = req.params.page ?? 1;
+  const pageSize = req.params.size ?? 10;
+  const all = await prisma.guest.findMany({
+    skip: (page-1)*pageSize,
+    take: pageSize
+  });
   res.json(all);
+})
+/**
+ * 按照 ID 查询游客信息
+ */
+router.get('/:id', async (req, res) => {
+  const guestID = +req.params.id;
+  const guest = await prisma.guest.findUnique({
+    where: {
+      id: guestID,
+    }
+  })
+  res.json(guest);
+})
+
+/**
+ * 修改游客信息, 通常是为了修改 State
+ */
+router.put<{id:string},{},IGuestUpdate>('/:id', async (req, res) => {
+  const guest = await prisma.guest.update({
+    where: {
+      id: +req.params.id
+    },
+    data: req.body
+  })
+  res.json(guest);
+})
+
+/**
+ * 删除游客信息
+ */
+router.delete('/:id', async (req, res) => {
+  const guestID = +req.params.id;
+  const guest = await prisma.guest.delete({
+    where: {
+      id: guestID,
+    }
+  })
+  res.json(guest);
 })
 
 export default router;
